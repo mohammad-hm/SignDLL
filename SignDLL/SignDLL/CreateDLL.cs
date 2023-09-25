b27c305be5e22b65d728f99f78f536e44b8db89f
mohammad hajimohammadi <hajimohammadi@amnpardaz.com>
2023-09-25 09:58:14 +0330

    createDLL class created

---
+++ SignDLL/SignDLL/CreateDLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SignDLL.CreateDLL;
+
+namespace SignDLL
+{
+    // when run project, the dll of this class created
+    internal class CreateDLL
+    {
+
+        public string DllName { get; set; } = "Example name";
+        public DateTimeOffset DllTime { get; set; } = new DateTimeOffset(2023, 2, 2, 2, 2, 2, TimeSpan.Zero);
+
+
+    }
+}
